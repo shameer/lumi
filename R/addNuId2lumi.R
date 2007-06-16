fc48ef41f3291abccb050d1bf2d7e2a03df4daef
dupan@northwestern.edu <dupan@northwestern.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-06-16 16:14:44 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/lumi@25428 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/addNuId2lumi.R
+++ R/addNuId2lumi.R
@@ -147,6 +147,7 @@ function(x.lumi, annotationFile=NULL, sep=NULL, lib=NULL, annotationColName=c(se
 	featureData <- featureData(x.lumi)
 	rownames(pData(featureData)) <- newId
 	featureData(x.lumi) <- featureData
+	if (!is.null(ib)) annotation(x.lumi) <- lib
 
 	## Add history tracking
 	history.finished <- as.character(Sys.time())
