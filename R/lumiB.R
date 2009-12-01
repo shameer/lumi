a30d4c730cee6c0b5b1a9d111599e42789b51a93
dupan@northwestern.edu <dupan@northwestern.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-12-01 17:04:43 +0000

    Updated lumiB to allow using user provided background correction method
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/lumi@43326 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/lumiB.R
+++ R/lumiB.R
@@ -1,6 +1,6 @@
 lumiB <- function(x.lumi, method = c('none', 'bgAdjust', 'forcePositive', 'bgAdjust.affy'), verbose=TRUE, ...) 
 {
-	method <- match.arg(method)
+	# method <- match.arg(method)
 	if (is(x.lumi, 'ExpressionSet')) {
 	    # x.lumi is a LumiBatch object
 	    x.matrix <- exprs(x.lumi)		
@@ -10,33 +10,39 @@ lumiB <- function(x.lumi, method = c('none', 'bgAdjust', 'forcePositive', 'bgAdj
 		stop('The object should be a matrix or class "ExpressionSet" inherited!')
 	}
 	
-	if (!(is.function(method)) && !(method %in% c('bgAdjust', 'none', 'forcePositive', 'bgAdjust.affy'))) {
-		cat('This method is not supported!\n')
-		return(x.lumi)
-	} else if (method == 'none') {
-		return(x.lumi)
-	} else if (method == 'forcePositive') {
-		if (min(x.matrix) > 0)  return(x.lumi)
+	if (!(is.function(method))) {
+		if (!(method %in% c('bgAdjust', 'none', 'forcePositive', 'bgAdjust.affy'))) {
+			cat('This method is not supported!\n')
+			return(x.lumi)
+		} else if (method == 'none') {
+			return(x.lumi)
+		} else if (method == 'forcePositive') {
+			if (min(x.matrix) > 0)  return(x.lumi)
+		}		
+		if (verbose) cat(paste('Perform', method, 'background correction ...\n'))
+	} else {
+		if (verbose) cat('Perform user provided background correction ...\n')		
 	}
 	
-	if (verbose) cat(paste('Perform', method, 'background correction ...\n'))
 	history.submitted <- as.character(Sys.time())
-	if (method == 'bgAdjust') {
-		x.matrix <- exprs(bgAdjust(x.lumi, ...))
-	} else if (method == 'bgAdjust.affy') {
-		x.matrix <- apply(x.matrix, 2, bg.adjust, ...) 
-	} else if (method == 'forcePositive') {
-		offset <- apply(x.matrix, 2, min)
-		offset[offset <= 0] <- offset[offset <= 0] - 1.01 	# to avoid higher fold-change when one value is less than 1
-		offset[offset > 0] <- 0
-		offset <- rep(1, nrow(x.matrix)) %*% t(offset)
-		x.matrix <- x.matrix - offset
-	} else if (is.function(method)) {
+	if (is.function(method)) {
 		x.matrix <- method(x.lumi, ...)
 		if (is(x.matrix, 'ExpressionSet')) x.matrix <- exprs(x.matrix)
 	} else {
-		cat('The method is not supported!\n')
-		return(x.lumi)
+		if (method == 'bgAdjust') {
+			x.matrix <- exprs(bgAdjust(x.lumi, ...))
+		} else if (method == 'bgAdjust.affy') {
+			x.matrix <- apply(x.matrix, 2, bg.adjust, ...) 
+		} else if (method == 'forcePositive') {
+			offset <- apply(x.matrix, 2, min)
+			offset[offset <= 0] <- offset[offset <= 0] - 1.01 	# to avoid higher fold-change when one value is less than 1
+			offset[offset > 0] <- 0
+			offset <- rep(1, nrow(x.matrix)) %*% t(offset)
+			x.matrix <- x.matrix - offset
+		} else {
+			cat('The method is not supported!\n')
+			return(x.lumi)
+		}
 	}
 
 	if (is(x.lumi, 'ExpressionSet')) {
