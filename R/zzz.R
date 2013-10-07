3b8fb9a816132d7a1e46f4f3f57243afdf580018
dupan@northwestern.edu <dupan@northwestern.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-10-07 21:47:50 +0000

    fixed some checking warnings
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/lumi@81250 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-.First.lib <- function(libname, pkgname) {
-    
-    if(.Platform$OS.type == "windows" && interactive()
-        && .Platform$GUI ==  "Rgui") {
-        addVigs2WinMenu("lumi")
-    }
-
-}
+# .onLoad <- function(libname, pkgname) {
+#     
+#     if(.Platform$OS.type == "windows" && interactive()
+#         && .Platform$GUI ==  "Rgui") {
+#         addVigs2WinMenu("lumi")
+#     }
+# 
+# }
